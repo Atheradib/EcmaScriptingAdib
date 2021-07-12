0529abdbdb0f19316c4152ea016ffb665336cb35
Atheradib <adib.rapha@gmail.com>
2021-07-12 13:33:12 +0530

    differences.js

---
+++ differences.js
@@ -0,0 +1,16 @@
+function GetDataVar(){
+    if(true){
+        var name="showbiz";
+    }
+    console.log(name);
+}
+GetDataVar();
+
+function GetDataLet(){
+    if(true){
+        let name="Ather";
+        console.log(name);
+    }
+    
+}
+GetDataLet();
