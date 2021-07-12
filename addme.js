a3e2e0889daf78b9cf675b3e0aafe4a5d926849a
Atheradib <adib.rapha@gmail.com>
2021-07-12 13:01:23 +0530

    addme.js

---
+++ addme.js
@@ -0,0 +1,10 @@
+function addme(num1,num2){
+var sum= num1 + num2;
+// console.log("sum of" + num1, "and" + num2 +"is equal to" +sum);
+// Literals 
+console.log(`sum of ${num1} and ${num2} is equal to ${sum}`);
+}
+
+addme(25,20);
+addme(25,25);
+addme(20,20);
