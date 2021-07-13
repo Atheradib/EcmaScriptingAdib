2372d41cf5993fd4fbc95f06c0370edbe0ab5921
Atheradib <adib.rapha@gmail.com>
2021-07-13 13:23:19 +0530

    revision

---
+++ revision.js
@@ -0,0 +1,12 @@
+function AddNumbersOpt(num1, num2){
+    return num1+num2;
+
+}
+
+console.log(AddNumbersOpt(5,8));
+
+console.log("sum of given numbers are"+ AddNumbersOpt(56,60));
+
+console.log("sum of 10 and 15 is" + AddNumbersOpt(10,15));
+
+console.log(`#################### sum of 30+60= ${AddNumbersOpt(30,60)}##########`);
