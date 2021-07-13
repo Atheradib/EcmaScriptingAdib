1dfbc4fc2b9ec7607fca0825b6929edcee2a8b59
Atheradib <adib.rapha@gmail.com>
2021-07-13 13:22:36 +0530

    biggernum

---
+++ Biggernum.js
@@ -0,0 +1,10 @@
+function BiggerNum(num1, num2){
+    if(num1>num2){
+        return num1;
+    } else{
+        return num2;
+    }
+}
+console.log(`Bigger number is ${BiggerNum(13,58)}`);
+console.log(`Bigger number is ${BiggerNum(133,58)}`);
+console.log(`Bigger number is ${BiggerNum(13,158)}`);
