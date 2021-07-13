30c34b4c3f6f2974349f58689db0bfcbc97dc524
Atheradib <adib.rapha@gmail.com>
2021-07-13 13:22:24 +0530

    arrow

---
+++ arrow.js
@@ -0,0 +1,17 @@
+const AddNumbersOpt=(num1,num2)=> num1+num2;
+const SubsNumbersOpt= (num1,num2)=>num1-num2;
+const MultNumbersOpt= (num1,num2)=> num1*num2;
+const GetData = ()=> This is Fat Arrow Function;
+
+console.log(GetData());
+
+
+//console.log(AddNumbersOpt(5,8));
+
+//console.log("sum of given numbers are"+ AddNumbersOpt(56,60));
+
+//console.log("sum of 10 and 15 is" + AddNumbersOpt(10,15));
+
+//console.log(`#################### sum of 30+60= ${AddNumbersOpt(30,60)} 30-60= ${SubsNumbersOpt(30,60)}
+//3*9=${MultNumbersOpt(3,9)}
+//##########`); 
