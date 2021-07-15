de3afbd163c29bde836f6a8eb1b69778b2aea39c
Atheradib <adib.rapha@gmail.com>
2021-07-15 12:09:21 +0530

    multiply

---
+++ amount.js
@@ -0,0 +1,6 @@
+const MillageCost =(distance,mileage,price) => 
+Math.round((distance/mileage)*price);
+
+// console.log(`Amount need to fill fuel is Rs. ${MillageCost(300,50,100)}`);
+console.log(`Amount need to fill fuel is Rs. ${MillageCost(320,51,110.08)}`);
+console.log(Math.floor(66.48));
---
+++ multiNum.js
@@ -0,0 +1,3 @@
+const BiggerNumMultiOpt = (num1,num2,num3,num4,num5,num6)  => Math.max(num1,num2,num3,num4,num5,num6)
+
+console.log(`Bigger Number is ${BiggerNumMultiOpt(23,56,75,21,45,88)}`);
