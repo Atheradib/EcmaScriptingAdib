0567d8d6578a6ea2efb8b89ce0e543444b4468c0
Atheradib <adib.rapha@gmail.com>
2021-07-15 14:36:58 +0530

    amount

--- amount.js
+++ amount.js
@@ -1,6 +1,8 @@
 const MillageCost =(distance,mileage,price) => 
-Math.round((distance/mileage)*price);
+Math.ceil((distance/mileage)*price);
 
 // console.log(`Amount need to fill fuel is Rs. ${MillageCost(300,50,100)}`);
 console.log(`Amount need to fill fuel is Rs. ${MillageCost(320,51,110.08)}`);
 console.log(Math.floor(66.48));
+
+
