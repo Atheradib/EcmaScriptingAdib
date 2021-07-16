c18d8d555769e57139b56628727c4be58eeea7ea
Atheradib <adib.rapha@gmail.com>
2021-07-16 12:53:26 +0530

    fencing

---
+++ fencingforany.js
@@ -0,0 +1,10 @@
+const FencingForAny = (edges,NumberOfWire,rate) => {
+    let sum = 0
+    for(let i=0; i<edges.length; i++){
+    sum = sum + edges[i];
+    }
+    return sum*NumberOfWire*rate;
+};
+
+
+console.log(`cost of fencing ${FencingForAny([10,15,45,48,14],5,10)}/-`);
