93dcd948dc0a621dc481c5614a92ae8765e7026b
Atheradib <adib.rapha@gmail.com>
2021-07-18 15:00:11 +0530

    18thjuly

--- fencingeven.js
+++ fencingeven.js
@@ -1,8 +1,10 @@
-const CollectionSumOfEven = (number) =>{
-    let sum=0;
+const CollectionSumOfEven = (number) => {
+    let sum = 0;
     number.forEach((element)=> {
         element % 2 === 0 && (sum = sum + element);
     });
+    return sum;
 }
 console.log(CollectionSumOfEven([5,2,,7,9,10,8,17,115,10]));
+
 
