453fa8bfc6dd408adc40cf01f9ad0a4bfbec21a9
Atheradib <adib.rapha@gmail.com>
2021-07-20 11:09:14 +0530

    practice

--- example1.js
+++ example1.js
@@ -1,1 +1,11 @@
-const EmployeeInfo = [{}]
+const EmployeeInfo = (data) => 
+    data.map((item)) => `I am planning to do ${item.course} from ${item.institute} situated in ${item.city} having mobile no. ${item.mobileno}`);
+
+    console.log( [{course:"PDAC" , institute:"Pickupbiz" , mobileNo:"9874561230", city:"Nanded"},
+    {course:"Module1" , institute:"Pickupbiz" , mobileNo:"9874561230", city:"pune"},
+
+    {course:"Module2" , institute:"Pickupbiz" , mobileNo:"9874561230", city:"HYD"},
+
+    {course:"Module3" , institute:"Pickupbiz" , mobileNo:"9874561230", city:"Mumbai"}
+
+        ]);
