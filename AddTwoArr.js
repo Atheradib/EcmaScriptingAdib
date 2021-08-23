755baa281a770ac9f0d27d2a0fc7c260d7f7ae9b
Atheradib <adib.rapha@gmail.com>
2021-08-23 15:35:32 +0530

    problems solve

---
+++ AddTwoArr.js
@@ -0,0 +1,3 @@
+const AddTwoArr = (arr1, arr2) => arr1.map((item, index) => item + arr2[index]);
+
+console.log(AddTwoArr([1, 2, 4, 8, 8], [8, 5, 4, 8, 6]));
---
+++ FindAllDuplicate.js
@@ -0,0 +1,9 @@
+const FindAllDuplicate = (arr) => {
+  const newArr = [];
+  arr.forEach((element, index) => {
+    arr.indexOf(element) !== index && newArr.push(element);
+  });
+  return newArr;
+};
+
+console.log(FindAllDuplicate([1, 5, 2, 4, 8, 45, 70, 8, 0, 70, 8, 1, 0, 4]));
