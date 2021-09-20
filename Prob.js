046dbff4af0e977e5b9c961406dac9faad1f68e0
Atheradib <adib.rapha@gmail.com>
2021-09-20 23:00:39 +0530

    Problemsolving

---
+++ Prob.js
@@ -0,0 +1,21 @@
+// const capthString = (str) => {
+//   const arrStr = str.split(" ");
+//   const newArr = [];
+//   arrStr.forEach((element) => {
+//     element.split("") && newArr.push(element.toUpperCase());
+//   });
+//   return newArr.join(" ");
+// };
+// console.log(capthString("my name is rohit"));
+
+// const arr = (arr1) => {
+//   let newArr = [];
+//   arr1.map((item) => {
+//     item === 0 && newArr.push(item);
+//   });
+//   arr1.map((item) => {
+//     item === 1 && newArr.push(item);
+//   });
+//   return newArr;
+// };
+// console.log(arr([1, 0, 1, 0, 1, 0]));
--- ReverseString.js
+++ ReverseString.js
@@ -19,13 +19,32 @@
 //   }
 //   return newArr.join("");
 // };
-const reverseWord = (str) => {
-  const arrStr = str.split("");
-  const newArr = [];
-  for (let i = arrStr.length - 1; i >= 0; i--) {
-    newArr.push(arrStr[i]);
-  }
-  return newArr.join("");
-};
+// const reverseWord = (str) => {
+//   const arrStr = str.split("");
+//   const newArr = [];
+//   for (let i = arrStr.length - 1; i >= 0; i--) {
+//     newArr.push(arrStr[i]);
+//   }
+//   return newArr.join("");
+// };
 
-console.log(`Expected Reverse word is ${reverseWord("Ather")}`);
+// console.log(`Expected Reverse word is ${reverseWord("Ather")}`);
+
+// const isVowel = (str) => ["a", "e", "i", "o", "u"].includes(str);
+
+// console.log(isVowel("o"));
+// console.log(isVowel("s"));
+
+const vowels = ["a", "e", "i", "o", "u", "y"];
+
+function isVowel(word) {
+  let result = false;
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i])) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+}
+console.log(isVowel("g"));
